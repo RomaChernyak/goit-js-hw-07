478edbb29b071536edca60748fb16bda51373e64
RomaChernyak <118313101+RomaChernyak@users.noreply.github.com>
2023-04-25 23:33:31 +0300

    tasks 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// console.log(galleryItems);
 
 function createGalleryItemsMarkup(galleryItems) {
     return galleryItems
@@ -12,24 +10,22 @@ function createGalleryItemsMarkup(galleryItems) {
             <li class="gallery__item">
                 <a class="gallery__link" href="${original}">
                     <img
-                    class="gallery__image"
-                    src="${preview}"
-                    data-source="${original}"
-                    alt="${description}"
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
                     />
                 </a>
             </li>
             `;
         })
-    .join('');
+        .join('');
 };
 
 const galleryItemsContainer = document.querySelector('.gallery');
 const galleryMarkup = createGalleryItemsMarkup(galleryItems);
 
 galleryItemsContainer.insertAdjacentHTML('beforeend', galleryMarkup);
-
-// 2. Реалізація делегування на ul.gallery і отримання url великого зображення.
 
 galleryItemsContainer.addEventListener('click', onGalleryContainerClick);
 
@@ -46,13 +42,6 @@ function onGalleryContainerClick(evt) {
 
     const galleryLinkEl = evt.target.closest('.gallery__link');
 
-    // galleryLinkEl.href;
-    // console.log(galleryLinkEl.href);
-
-    // 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-
-    // 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-
     const instance = basicLightbox.create(`
         <img src="${galleryLinkEl.href}" width="800" height="600">
     `)
@@ -60,8 +49,6 @@ function onGalleryContainerClick(evt) {
     instance.show()
 
     galleryItemsContainer.addEventListener('keydown', (evt) => {
-        // console.log(evt.code);
-        
         if (evt.code === 'Escape') {
             instance.close();
         }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. Використовуй готовий код з першого завдання.
+// console.log(galleryItems);
 
 function createGalleryItemsMarkup(galleryItems) {
     return galleryItems
@@ -12,15 +10,15 @@ function createGalleryItemsMarkup(galleryItems) {
             <li class="gallery__item">
                 <a class="gallery__link" href="${original}">
                     <img
-                    class="gallery__image"
-                    src="${preview}"
-                    alt="${description}"
+                        class="gallery__image"
+                        src="${preview}"
+                        alt="${description}"
                     />
                 </a>
             </li>
             `;
         })
-    .join('');
+        .join('');
 };
 
 const galleryItemsContainer = document.querySelector('.gallery');
@@ -28,14 +26,10 @@ const galleryMarkup = createGalleryItemsMarkup(galleryItems);
 
 galleryItemsContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-// 2. Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
 
-
-
-// 3. Ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery. Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
-
-const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
-
-// 4. Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
 
 
